d92b963a7e5f6b67a717f07186503c3297f6dfda
orlandoburgoscabarcas <orlandoburgoscabarcas1@gmail.com>
2026-05-21 09:48:08 -0500

    Adelanto de las casillas y el tamaño de la pagina

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
 CASILLA = 60
 
-for fila in range(8):
-    for col in range(8):
-        if (fila + col) % 2 == 0:
-            color = BLANCO
-        else:
-            color = NEGRO
-        
-        pygame.draw.rect(pantalla, color, (col*CASILLA, fila*CASILLA, CASILLA, CASILLA))
- 
-pygame.display.update()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
 
-input() 
+    for fila in range(8):
+        for col in range(8):
+            if (fila + col) % 2 == 0:
+                color = BLANCO
+            else:
+                color = NEGRO
+
+            pygame.draw.rect(
+                pantalla,color,
+                (col * CASILLA, fila * CASILLA, CASILLA, CASILLA)
+            )
+
+    pygame.display.update()
