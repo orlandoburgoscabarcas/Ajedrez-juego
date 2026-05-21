54df7b15d968346d1937be928558cb5d806588ed
orlandoburgoscabarcas <orlandoburgoscabarcas1@gmail.com>
2026-05-21 09:34:23 -0500

    Pantalla y casillas del ajedrez

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hola mundo, voy a crear mi juego")
+import pygame
+pygame.init()
+
+pantalla = pygame.display.set_mode((480, 480))
+
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+CASILLA = 60
+
+for fila in range(8):
+    for col in range(8):
+        if (fila + col) % 2 == 0:
+            color = BLANCO
+        else:
+            color = NEGRO
+        
+        pygame.draw.rect(pantalla, color, (col*CASILLA, fila*CASILLA, CASILLA, CASILLA))
+ 
+pygame.display.update()
+
+input() 
